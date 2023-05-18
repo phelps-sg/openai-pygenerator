69b82b5e46cbb473a94ecfe26e9b7bddd54906c0
Steve Phelps <sphelps@sphelps.net>
2023-05-18 10:06:27 +0100

    move project metadata to setup.cfg

--- setup.py
+++ setup.py
@@ -1,9 +1,2 @@
 from setuptools import setup
-
-setup(
-    name='openai_pygenerator',
-    use_scm_version=True,
-    setup_requires=['setuptools_scm'],
-    py_modules=['openai_pygenerator'],
-    package_dir={'': 'src'},
-)
+setup(use_scm_version=True, setup_requires=['setuptools_scm'])
